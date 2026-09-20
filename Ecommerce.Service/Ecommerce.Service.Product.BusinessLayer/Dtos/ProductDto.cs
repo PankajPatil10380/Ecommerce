b3b9f72c4641ca4a672d47b1675dfd72ede73f97
Pankaj Patil <pankaj.patil@infocratsweb.com>
2026-09-20 15:06:00 +0530

    normal commit

--- Ecommerce.Service/Ecommerce.Service.Product.BusinessLayer/Dtos/ProductDto.cs
+++ Ecommerce.Service/Ecommerce.Service.Product.BusinessLayer/Dtos/ProductDto.cs
@@ -5,7 +5,7 @@ namespace Ecommerce.Service.Product.BusinessLayer.Dtos
     public class ProductDto
     {
         public int Id { get; set; }
-        public string ProductName { get; set; } = string.Empty;
+        public string ProductName { get; set; }
         public string? Description { get; set; }
         public decimal Price { get; set; }
         public int CategoryId { get; set; }
